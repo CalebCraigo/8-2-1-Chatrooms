e35c53020fbe7c78b54f147fd1a3b9e31305ee82
CalebCraigo <CalebDOS1@yahoo.com>
2019-10-20 15:27:28 -0400

    updated user sign up and log in functionality

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse, HttpResponseRedirect
 
 
 from .models import Profile
@@ -9,8 +11,19 @@ from .forms import CustomUserCreationForm
 
 class SignUpView(generic.CreateView):
     form_class = CustomUserCreationForm
-    success_url = reverse_lazy('rooms:list')
+    success_url = reverse_lazy('accounts:new_profile')
     template_name = 'signup.html'
+
+    def form_valid(self, form):
+        #save the new user first
+        form.save()
+        #get the username and password
+        username = self.request.POST['username']
+        password = self.request.POST['password1']
+        #authenticate user then login
+        user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password1'],)
+        login(self.request, user)
+        return HttpResponseRedirect(self.success_url)
 
 
 class LogOutView(generic.TemplateView):
